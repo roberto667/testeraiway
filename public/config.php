99c4b2bc07aac22e5e7d09c84edb7df7138b658e
Roberto <112992674+roberto667@users.noreply.github.com>
2026-01-09 17:18:46 -0300

    Refactor Conexao class for environment variable usage
    Updated the Conexao class to use environment variables for database connection parameters.

--- public/config.php
+++ public/config.php
@@ -6,20 +6,24 @@ class Conexao
     public static function conectar()
     {
         if (!isset(self::$con)) {
-            $serv = "localhost";
-            $user = "root";
-            $senha = "";
-            $db = "rest_doc_web";
 
-            self::$con = new mysqli($serv, $user, $senha, $db);
+            $host = getenv('MYSQLHOST');
+            $port = getenv('MYSQLPORT');
+            $db   = getenv('MYSQLDATABASE');
+            $user = getenv('MYSQLUSER');
+            $pass = getenv('MYSQLPASSWORD');
+
+            self::$con = new mysqli($host, $user, $pass, $db, $port);
 
             if (self::$con->connect_error) {
-                echo json_encode(["erro" => "Conexão falhou: " . self::$con->connect_error]);
+                header('Content-Type: application/json');
+                echo json_encode(["erro" => "Falha ao conectar no banco"]);
                 exit;
             }
+
+            self::$con->set_charset("utf8mb4");
         }
 
         return self::$con;
     }
 }
-
